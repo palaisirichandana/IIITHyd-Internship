c749ff22820938ba02ada30a8a2be26f96f0e4e7
Shresth <7shresthagarwal@gmail.com>
2025-06-13 23:25:16 +0530

    Create train.py

---
+++ task3/train.py
@@ -0,0 +1,4 @@
+from ultralytics import YOLO
+
+model = YOLO('yolo11n.pt')  
+model.train(data="C:/Users/Shresth Agarwal/OneDrive/Documents/AIML training/Internship/task3/african-wildlife.yaml", epochs=10 , imgsz=640, batch=4, cache = True)
